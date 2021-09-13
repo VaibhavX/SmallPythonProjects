d2dfa63943e6d03f783c22fa5cad5fb4ca440f90
Vaibhav Vishal <vaibhavx@Vaibhavs-MacBook-Pro.local>
2021-09-13 12:22:54 -0700

    bagels.py python program

---
+++ bagels.py
@@ -0,0 +1,4 @@
+#Bagels Program - 10 tries to guess secret 3-digit number based on clues.
+#"Pico" when guess has correct digit in wrong place
+#"Fermi" when guess has correct digit in the correct place
+#"Bagels" when guess has no correct digits
