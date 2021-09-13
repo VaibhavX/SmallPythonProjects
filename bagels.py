6e19ad4a8fc731b825bc4674fa24bb151cd8e67b
Vaibhav Vishal <vaibhavx@Vaibhavs-MacBook-Pro.local>
2021-09-13 12:39:23 -0700

    New Changes

--- bagels.py
+++ bagels.py
@@ -2,3 +2,11 @@
 #"Pico" when guess has correct digit in wrong place
 #"Fermi" when guess has correct digit in the correct place
 #"Bagels" when guess has no correct digits
+
+import random
+
+NUM_DIGITS = 3
+MAX_GUESSES = 10
+
+def main():
+    print("Bagels Game")
